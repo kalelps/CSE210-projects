3eb5f6336cdb51293e63d8e293804cdc8b1a0027
Gabriel Perez Sanchez <kalel.perez.sanchez@gmail.com>
2025-11-21 21:56:46 -0500

    Add files via upload

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -8,24 +8,24 @@ class Program
         List<Video> videos = new List<Video>();
 
         // Video 1
-        Video video1 = new Video("How to Cook Pasta", "Chef Mario", 300);
+        Video video1 = new Video("How to Cook Pasta", "Chef Mario", 657);
         video1.AddComment(new Comment("Anna", "Great recipe!"));
         video1.AddComment(new Comment("Luis", "Tried it today, delicious!"));
-        video1.AddComment(new Comment("Maria", "Can you make a vegan version?"));
         videos.Add(video1);
 
         // Video 2
-        Video video2 = new Video("Learn C# in 10 Minutes", "TechGuru", 600);
+        Video video2 = new Video("Learn HTML in 10 Minutes", "TechWizard2020", 667);
         video2.AddComment(new Comment("John", "Very helpful tutorial."));
         video2.AddComment(new Comment("Sara", "Thanks for explaining clearly."));
         video2.AddComment(new Comment("Peter", "I finally understand classes!"));
+        video2.AddComment(new Comment("Harold", "Can you make a video on CSS next?"));
         videos.Add(video2);
 
         // Video 3
-        Video video3 = new Video("Traveling in Japan", "AdventureTime", 900);
-        video3.AddComment(new Comment("Alex", "Japan looks amazing!"));
-        video3.AddComment(new Comment("Kevin", "I want to visit Mount Fuji."));
-        video3.AddComment(new Comment("Lily", "The food shots were incredible."));
+        Video video3 = new Video("Traveling in Peru, Cusco", "GaryTraveler", 900);
+        video3.AddComment(new Comment("Alex", "Peru looks amazing!"));
+        video3.AddComment(new Comment("Kelvin", "I want to visit Machu Picchu."));
+        video3.AddComment(new Comment("Lily", "The food shots were incredible!"));
         videos.Add(video3);
 
         // Display all video info
